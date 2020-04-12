74f8877384387c8140864c6f83f49942fa379dba
徐涛Tory <xutao@theduapp.com>
2020-04-12 21:31:28 +0800

    修改fetchUrl错误问题

--- manifest_xml.py
+++ manifest_xml.py
@@ -93,7 +93,9 @@ class _XmlRemote(object):
     # We handle no scheme by replacing it with an obscure protocol, gopher
     # and then replacing it with the original when we are done.
 
-    if manifestUrl.find(':') != manifestUrl.find('/') - 1:
+    if url.startswith("git@"):
+        pass
+    elif manifestUrl.find(':') != manifestUrl.find('/') - 1:
       url = urllib.parse.urljoin('gopher://' + manifestUrl, url)
       url = re.sub(r'^gopher://', '', url)
     else:
